96477c99443058833bafe36527f1272e07d9facb
arszky <ariskyyy@gmail.com>
2022-03-26 13:53:35 +0700

    loop tracks

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Form from './components/Card/form'
+import Home from './page/home'
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 
-const Card=({title,artists,img})=>{
+const Card = ({ title, artists, img }) => {
   return (
-    <div className='card'>
-      <div className='card-wrap'>
-        <img src={img} alt={title} className="card_img"/>
+    <div className="card">
+      <div className="card-wrap">
+        <img src={img} alt={title} className="card_img" />
 
-        <h3 className='card_album'>{title}</h3>
-        <h3 className='card_artist'>{artists}</h3>
+        <div class="card_content">
+          <h3 className="card_album">{title}</h3>
+          <h3 className="card_artist">{artists}</h3>
+        </div>
 
         <div className="button_wrapper">
           <button className="card_button">Select</button>
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Card;
